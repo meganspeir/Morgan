92e546d44dfeff6d65695ab9d3e9939c32e35df0
Megan Speir <no-reply@meganspeir.com>
2013-07-31 16:48:01 -0700

    manipulate twitter data

--- mytwitter.py
+++ mytwitter.py
@@ -1,11 +1,16 @@
 from twython import Twython
+import json
 
-APP_KEY = 'FILL IN'
-APP_SECRET = 'FILL IN'
+APP_KEY = 'HIDDEN'
+APP_SECRET = 'HIDDEN'
 
 twitter = Twython(APP_KEY, APP_SECRET, oauth_version=2)
 ACCESS_TOKEN = twitter.obtain_access_token()
 
 twitter = Twython(APP_KEY, access_token=ACCESS_TOKEN)
 
-print twitter.search(q='python')
+# print twitter.search(q='python')
+search = twitter.search(q='#oakmtg :(', result_type='mixed')
+for entry in search['statuses']:
+    print entry
+
